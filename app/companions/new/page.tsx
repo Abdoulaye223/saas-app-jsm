c0995ab251343538368fed5576412c74cb4eab58
Abdoulaye223 <abdoulayeisimage@gmail.com>
2025-05-29 00:39:19 +0000

    Implementation Of Supabase

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,6 +1,13 @@
 import CompanionForm from "@/components/CompanionForm";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+// This page is used to create a new companion. It checks if the user is authenticated and then renders the CompanionForm component.
 
-const NewCompanion = () => {
+const NewCompanion = async () => {
+    const { userId} = await auth();
+    if(!userId) redirect("/sign-in");
+
+
     return (
         <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
             <article className="w-full gap-4 flex flex-col">
